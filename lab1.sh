5f52fc2464db204bb7afb9b1ce62bf3b917da6f4
ViktoriaGatz <47521227+ViktoriaGatz@users.noreply.github.com>
2020-09-11 19:41:08 +0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -6,7 +6,7 @@ then
 	echo "Нет параметров, воспользуйтесь --help|-h"
 	exit 1
 fi
-
+# OPTIND, OPTARG
 # Двоеточие после имени опции в списке означает, что для неё
 # обязателен аргумент
 while getopts "hk:-:au:d:so:p:m:g:" opt
@@ -14,12 +14,21 @@ do
 	case ${opt} in
 	-) case "${OPTARG}" in
 		help) echo "Здесь будет хелп инфо (-help)";;
-	esac;;
-	h) echo "Здесь будет хелп инфо (-h)";;
-	k) echo "Вызвана команда -${opt} kill"
-		echo "Убийство процесса который занимает порт ${OPTARG}"
-		kill -9 ${OPTARG}
-		;;
+		esac;;
+	h) echo "Здесь будет хелп инфо (--help|-h)"
+		echo "${OPTIND}"
+		echo "Авторы: Виктория Гацуля, Сапронов Евгений"
+		echo "Доступные аргументы: "
+		echo -e "-a\tall\tвывод всех сетевых интерфейсов"
+		echo -e "-u\tup\tвключение сетевого интерфейса"
+		echo -e "-d\tdown\tвывод всех сетевых интерфейсов"
+		echo -e "-p\tip\tвывод всех сетевых интерфейсов"
+		echo -e "-m\tmask\tвывод всех сетевых интерфейсов"
+		echo -e "-g\tgateway\tвывод всех сетевых интерфейсов"
+		echo -e "-k\tkill\tвывод всех сетевых интерфейсов"
+		echo -e "-o\totcl\tвывод всех сетевых интерфейсов"
+		echo -e "-s\tstat\tвывод всех сетевых интерфейсов"
+		;;		
 	a) echo "Вывод всех сетевых интерфейсов"
 		ifconfig -a
 		;;
@@ -44,14 +53,16 @@ do
 		read my_gateway;
 		# route add ${OPTARG} gw ${my_gateway}
 		;;
-		
+	k) echo "Вызвана команда -${opt} kill"
+		echo "Убийство процесса который занимает порт ${OPTARG}"
+		kill -9 ${OPTARG}
+		;;		
 	o) echo "Отключение сетевого интерфейса по шаблону IP: ${OPTARG}"
 		n=$(ip addr | grep ${OPTARG} | wc -w)
 		name=$(ip addr | grep ${OPTARG} | gawk '{print $'${n}'}')
 		ifconfig ${name} down
 		;;
-	s) echo "Статистика по сетевым интерфейсам: "
-		ip -s link
+	s) echo "Сетевая статистика: "
 		;;
 	# Если опция была неизвестной
 	*) echo "Воспользуйтесь --help|-h";;
